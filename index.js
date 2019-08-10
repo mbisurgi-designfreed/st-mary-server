dc182fe7d1a6c34e6f9209d09ec5fcfabd3ba5db
mbisurgi-designfreed <maximiliano@designfreed.com>
2019-08-10 18:20:19 -0300

    add env variable

--- index.js
+++ index.js
@@ -18,31 +18,6 @@ app.post('/email', async (req, res) => {
     const {edad, email, nombre, seconds} = req.body;
     
     try {
-    //     const response = await axios.post('https://api.sendgrid.com/v3/mail/send', {
-    //         "personalizations": [
-    //             {
-    //             "to": [
-    //                 {
-    //                 "email": "mbisurgi@bc-group.com.ar"
-    //                 }
-    //             ],
-    //             "subject": "Encuesta Test"
-    //             }
-    //         ],
-    //         "from": {
-    //             "email": "mbisurgi@bc-group.com.ar"
-    //         },
-    //         "content": [
-    //             {
-    //             "type": "text/plain",
-    //             "value": `${nombre} de ${edad} años de edad, cuyo mail es ${email} tardo ${seconds} en resolver el problema`
-    //             }
-    //         ]
-    //     }, {headers: {
-    //         'Authorization': 'Bearer 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',
-    //         'Content-Type': 'application/json'
-    //     }})
-
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -52,10 +27,10 @@ app.post('/email', async (req, res) => {
        });
 
     const mailOptions = {
-        from: 'maximiliano.bisurgi@gmail.com', // sender address
-        to: 'maximiliano.bisurgi@gmail.com', // list of receivers
-        subject: 'Subject of your email', // Subject line
-        html: `<p>${nombre} de ${edad} años de edad, cuyo mail es ${email} tardo ${seconds} en resolver el problema</p>`// plain text body
+        from: 'maximiliano.bisurgi@gmail.com', 
+        to: process.env.TO || 'maximiliano.bisurgi@gmail.com',
+        subject: `Resultado encuesta de ${email}`, 
+        html: `<p>${nombre} de ${edad} años de edad, cuyo mail es ${email} tardo ${seconds} en resolver el problema</p>`
       };
 
       await new Promise((resolve, reject) => {
